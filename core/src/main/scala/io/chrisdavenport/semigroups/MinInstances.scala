e8d7ea1bd6d0b544c3195cd99f9e7688766c757b
danicheg <e.danicheg@yandex.ru>
2021-09-06 21:46:30 +0300

    Use opaque types for Min

--- core/src/main/scala/io/chrisdavenport/semigroups/MinInstances.scala
+++ core/src/main/scala/io/chrisdavenport/semigroups/MinInstances.scala
@@ -1,16 +1,13 @@
 package io.chrisdavenport.semigroups
 
 import cats._
-import cats.implicits._
+import cats.syntax.all._
 import cats.kernel.{Semilattice, BoundedSemilattice, UpperBounded}
 
-final case class Min[A](getMin: A) extends AnyVal
-object Min extends MinInstances {
-  def min[F[_]: Reducible, A](fa: F[A])(implicit L: Semigroup[Min[A]]): A = 
-    fa.reduceMap(Min(_)).getMin
-}
-
 private[semigroups] trait MinInstances extends MinInstances1 {
+  def min[F[_]: Reducible, A](fa: F[A])(implicit L: Semigroup[Min[A]]): A =
+    fa.reduceMap(Min(_)).getMin
+
   implicit def orderedMinBoundedSemilattice[A: UpperBounded: Order]: BoundedSemilattice[Min[A]] = new BoundedSemilattice[Min[A]] {
     def combine(x: Min[A], y: Min[A]): Min[A] = Min(Order[A].min(x.getMin, y.getMin))
     def empty: Min[A] = Min(UpperBounded[A].maxBound)
@@ -18,14 +15,12 @@ private[semigroups] trait MinInstances extends MinInstances1 {
 }
 
 private[semigroups] trait MinInstances1  {
-
   implicit def minShow[A: Show]: Show[Min[A]] = 
-    Show.show[Min[A]](minA => show"Min(${minA.getMin})")
+    Show.show[Min[A]](minA => s"Min(${minA.getMin.show})")
 
   implicit def minOrder[A: Order]: Order[Min[A]] =
     Order.by(_.getMin)
 
-  
   implicit def orderedMinSemilattice[A: Order]: Semilattice[Min[A]] = new Semilattice[Min[A]]{
     def combine(x: Min[A], y: Min[A]): Min[A] = 
       Min(Order.min(x.getMin, y.getMin))
@@ -38,9 +33,9 @@ private[semigroups] trait MinInstances1  {
 
       @scala.annotation.tailrec
       def tailRecM[A, B](a: A)(f: A => Min[Either[A,B]]): Min[B] =
-        f(a) match {
-          case Min(Left(a)) => tailRecM(a)(f)
-          case Min(Right(b)) => Min(b)
+        f(a).getMin match {
+          case Left(a) => tailRecM(a)(f)
+          case Right(b) => Min(b)
         }
       // Members declared in cats.Foldable
       def foldLeft[A, B](fa: Min[A],b: B)(f: (B, A) => B): B = 
