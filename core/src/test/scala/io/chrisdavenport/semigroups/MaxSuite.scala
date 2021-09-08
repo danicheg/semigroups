98f4fe20dd5a5998469300ff0bcaf23bda100bed
danicheg <e.danicheg@yandex.ru>
2021-09-08 19:48:06 +0300

    Use munit in MaxSuite

--- core/src/test/scala/io/chrisdavenport/semigroups/MaxSuite.scala
+++ core/src/test/scala/io/chrisdavenport/semigroups/MaxSuite.scala
@@ -3,13 +3,9 @@ package io.chrisdavenport.semigroups
 import cats._
 import cats.kernel.laws.discipline._
 import cats.laws.discipline._
-import cats.implicits._
-import org.scalatest.funsuite.AnyFunSuite
-import org.typelevel.discipline.scalatest.FunSuiteDiscipline
-import org.scalatest.matchers.should.Matchers
-import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import cats.syntax.all._
 
-class MaxTests extends AnyFunSuite with SemigroupsArbitraries with FunSuiteDiscipline with Matchers with ScalaCheckDrivenPropertyChecks{
+class MaxSuite extends munit.DisciplineSuite with SemigroupsArbitraries {
   checkAll("Max", OrderTests[Max[Int]].order)
   checkAll("Max", BoundedSemilatticeTests[Max[Int]].boundedSemilattice)
   checkAll("Max", MonadTests[Max].monad[Int, Int, Int])
@@ -17,14 +13,13 @@ class MaxTests extends AnyFunSuite with SemigroupsArbitraries with FunSuiteDisci
   checkAll("Max", DistributiveTests[Max].distributive[Int, Int, Int, Option, Id])
 
   test("show"){
-    Max(true).show shouldEqual "Max(true)"
-    Max(false).show shouldEqual "Max(false)"
+    assertEquals(Max(true).show, "Max(true)")
+    assertEquals(Max(false).show, "Max(false)")
   }
 
   test("returns Larger"){
     val first = Max(1)
     val second = Max(3)
-    first |+| second shouldEqual second
+    assertEquals(first |+| second, second)
   }
-
 }
